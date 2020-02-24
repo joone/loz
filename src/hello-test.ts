bfd4bb4e0e211c030b1332528dbb3dcee785b9b3
Joone Hur <joone@kldp.org>
2020-02-23 16:37:37 -0800

    Add support for unit test with mocha and chai
    $ npm run test

---
+++ src/hello-test.ts
@@ -0,0 +1,5 @@
+export function hello() {
+  return true;
+}
+
+export default hello;
---
+++ src/tests/test.ts
@@ -0,0 +1,11 @@
+import { hello } from '../hello-test';
+import { expect } from 'chai';
+import 'mocha';
+
+describe('First test', 
+  () => { 
+    it('should return true', () => { 
+      const result = hello();
+      expect(result).to.equal(true); 
+  }); 
+});

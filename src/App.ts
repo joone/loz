7c2ca9d6e909e9d7d757e08852679683dd45e6fb
Joone Hur <joone.hur@gmail.com>
2021-10-20 21:35:55 -0700

    Set strict in compile options

--- src/App.ts
+++ src/App.ts
@@ -1,7 +1,7 @@
 import * as express from 'express'
 
 class App {
-  public express 
+  public express : any
 
   constructor () {
     this.express = express()
--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,6 @@ import * as config from "./config.json";
 
 const port = process.env.PORT || config.server.port
 
-app.listen(port, (err) => {
-  if (err) {
-    return console.log(err)
-  }
-
+app.listen(port, () => {
   return console.log(`server is listening on ${port}`)
 })

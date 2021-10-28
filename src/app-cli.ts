70a32487c5fe242dfc666760c17227fd81ff221f
Joone Hur <joone.hur@gmail.com>
2021-10-28 15:38:01 -0700

    Add a CLI
    $ node dist/app-cli.js --api getPost
    { status: 'success', message: 'Post retrieved successfully' }

---
+++ src/app-cli.ts
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+import * as yargs from 'yargs';
+import * as postAPI from "./api"
+
+// node dist/app-cli.js --help
+// node dist/app-cli.js --api getPost
+const parser = yargs(process.argv.slice(2)).options({
+  api: { type: 'string', description: "getPost API"  },
+}).argv;
+
+(async() => {
+  const argv = await parser
+ 
+  if (argv.api) {
+    if (argv.api == "getPost")
+      console.log(postAPI.getPost())
+  }
+})();
+
+
+
+
+

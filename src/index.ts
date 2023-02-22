08480b2f25f111233e53eca3a631089f40680c70
Joone Hur <joone.hur@gmail.com>
2023-02-22 10:20:51 -0800

    Implement a ChatGPT CLI

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,40 @@
-import app from "./app";
-import * as config from "./config.json";
+const { Configuration, OpenAIApi } = require("openai");
+require('dotenv').config()
 
-const port = process.env.PORT || config.server.port;
+// get runtime parameters
+const runtimeArgs = process.argv.slice(2);
 
-app.listen(port, () => {
-  return console.log(`server is listening on ${port}`);
+// print the runtime parameters
+console.log('Runtime parameters:', runtimeArgs);
+
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
 });
+
+const openai = new OpenAIApi(configuration);
+
+async function runCompletion (prompt : string) {
+  if (prompt === undefined)
+    return;
+
+  const completion = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: prompt,
+    temperature: 0,
+    max_tokens: 60,
+    top_p: 1.0,
+    frequency_penalty: 0.0,
+    presence_penalty: 0.0
+  });
+  console.log(completion.data.choices[0].text);
+}
+
+(async () => {
+  let prompt = runtimeArgs[0];
+  if (prompt !== undefined) {
+    prompt = "Could you please rephrase the following sentence to make it sound more natural?: " + prompt;
+    await runCompletion(prompt);
+  } else
+    console.info("Please input your prompt");
+})();

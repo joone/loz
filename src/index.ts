1936c7d9d19314e142f0fee1009369617e387c71
Joone Hur <joone.hur@gmail.com>
2023-02-24 19:27:27 -0800

    Add support for interactive mode

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,7 @@ const openai = new OpenAIApi(configuration);
 
 async function runCompletion(settings: any) {
   const completion = await openai.createCompletion(settings);
+  console.log(completion.data);
   console.log(completion.data.choices[0].text);
 }
 
@@ -26,11 +27,9 @@ const args = yargs
     });
   })
   .options({
-    headless: {
-      type: "boolean",
-      alias: "h",
-      describe: "run your program",
-      default: true,
+    interactive: {
+      alias: "i",
+      describe: "interactive mode",
     },
   })
   .help()
@@ -42,6 +41,7 @@ const args = yargs
     temperature: number,
     max_tokens: number,
     top_p: number,
+    stream: boolean,
     frequency_penalty: number,
     presence_penalty: number,
     stop?: string[],
@@ -53,6 +53,7 @@ let defaultSettings: GPTSettings = {
   temperature: 0,
   max_tokens: 60,
   top_p: 1.0,
+  stream: false,
   frequency_penalty: 0.0,
   presence_penalty: 0.0,
 };
@@ -76,12 +77,34 @@ let defaultSettings: GPTSettings = {
       break;
     }
     default: {
-      // Answer questions based on existing knowledge.
-      if (args.sentence === undefined && args.service !== undefined) {
-        const prompt = "" + args.service;
-        defaultSettings.prompt = prompt
-        defaultSettings.max_tokens = 4000;
-        await runCompletion(defaultSettings);
+      // Interactive mode
+      if (args.sentence === undefined && args.service == undefined) {
+        console.log("Please input your prompt:");
+        const  waitingSymbol = '> ';
+
+        while(true) {
+          // wait for user input and run completion
+          const prompt : any = await new Promise((resolve) => {
+            const readline = require("readline").createInterface({
+              input: process.stdin,
+              output: process.stdout,
+            });
+            readline.question(waitingSymbol, (text: string) => {
+              readline.close();
+              resolve(text);
+            });
+          });
+          if (prompt === 'exit' || prompt === 'quit') {
+            console.log("Goodbye!");
+            break;
+          }
+          defaultSettings.prompt = prompt
+          defaultSettings.max_tokens = 4000;
+          //defaultSettings.stream = true;
+         
+          await runCompletion(defaultSettings);
+
+        }
       } else {
         console.info("Please input your prompt");
       }

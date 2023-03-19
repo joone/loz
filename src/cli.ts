2aeb957266b270bf31a752fb7d1e250b9c613aa3
Joone Hur <joone.hur@gmail.com>
2023-03-19 01:30:00 -0700

    Update prompt description and add git commit command
    Update prompt description and add logic to run git commit command when prompt is set to 'commit'.

--- src/cli.ts
+++ src/cli.ts
@@ -6,7 +6,7 @@ const args = yargs
   .wrap(null)
   .command("$0 [prompt]", "Luv: a simple ChatGTP CLI tool", (yargs) => {
     yargs.positional("prompt", {
-      description: "Prompt to start the conversation",
+      description: "Prompt to answer",
       type: "string",
     });
   })
@@ -37,12 +37,12 @@ const args = yargs
 (async () => {
   let loz = new Loz();
   if (args.prompt !== undefined) {
-    loz.answerAnyQuestion(args.prompt as string);
+    if (args.prompt === "commit") loz.runGitCommit();
+    else loz.answerAnyQuestion(args.prompt as string);
   } else if (args.git !== undefined) {
     loz.writeGitCommitMessage();
-  } else if (args.commit !== undefined) {
-    loz.runGitCommit();
   } else {
+    console.log("Luv: a simple ChatGTP CLI tool");
     loz.runPromptIntractive();
   }
 })();

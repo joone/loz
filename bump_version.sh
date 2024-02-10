bf79861e1904f24857c5ed846fca61817b7e4213
Joone Hur <joone.hur@gmail.com>
2024-02-10 01:27:12 -0800

    Bump version to %s
    Generated by gpt-3.5-turbo

--- bump_version.sh
+++ bump_version.sh
@@ -1,40 +1,10 @@
 #!/bin/bash
+npm version patch -m "Bump version to %s"
 
-# Find the highest tag nuber
-VERSION=`git describe --abbrev=0 --tags`
-
-# Replace dots with spaces to split into an array
-VERSION_BITS=(${VERSION//./ })
-
-# Increment the digit
-VNUM1=${VERSION_BITS[0]}
-VNUM2=${VERSION_BITS[1]}
-VNUM3=${VERSION_BITS[2]}
-VNUM3=$((VNUM3+1))
-
-# Create a new tag
-NEW_TAG="$VNUM1.$VNUM2.$VNUM3"
-
-if [ "$1" ]; then
-  NEW_TAG=$1
-fi
-
-echo "Updating $VERSION to $NEW_TAG"
-
-sed -i "0,/$VERSION/s//$NEW_TAG/" package.json
-sed -i "0,/$VERSION/s//$NEW_TAG/" package-lock.json
-
-git add package.json
-git add package-lock.json
-git commit -m "Release $NEW_TAG"
-
-echo "Would you like to push the changes? (y or n)"
+echo "Would you like to push the tag? (y or n)"
 read answer
 
 if [ "$answer" == "y" ]; then
-  git push origin
-  git tag $NEW_TAG
   git push --tags
   exit 0
 fi
-
